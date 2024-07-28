75cd6fb84710b170c38d4a17c90c8082d29417f2
KKIMYENN <yenny031003@gmail.com>
2024-07-28 18:44:05 +0900

    달력 QA

--- src/Pages/Materials/PrevMaterial.jsx
+++ src/Pages/Materials/PrevMaterial.jsx
@@ -1,14 +1,46 @@
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "./CustomCalendar.css";
+import moment from "moment";
 import styled from "styled-components";
 import { ReactComponent as DownloadIcon } from "../../Assets/DownloadIcon.svg";
-import { ReactComponent as Calendar } from "../../Assets/Calendar.svg";
+import { ReactComponent as CalendarIcon } from "../../Assets/Calendar.svg";
 
 const PrevMaterial = () => {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    setShowCalendar(false);
+  };
+
+  const toggleCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
+
   return (
     <MainDiv>
       <Control>
-        <div>날짜</div>
-        <Calendar />
+        <div>{selectedDate.toLocaleDateString()}</div>
+        <CalendarIcon onClick={toggleCalendar} />
       </Control>
+      {showCalendar && (
+        <CalendarOverlay onClick={toggleCalendar}>
+          <CalendarContainer onClick={(e) => e.stopPropagation()}>
+            <Calendar
+              onChange={handleDateChange}
+              value={selectedDate}
+              formatDay={(locale, date) => moment(date).format("DD")}
+              tileClassName={({ date, view }) =>
+                date.getTime() === selectedDate.getTime()
+                  ? "selected-date"
+                  : null
+              }
+            />
+          </CalendarContainer>
+        </CalendarOverlay>
+      )}
       <ItemBox>
         자료 이름
         <DownloadIcon />
@@ -34,7 +66,32 @@ const Control = styled.div`
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
+  cursor: pointer;
 `;
+
+const CalendarOverlay = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+const CalendarContainer = styled.div`
+  position: fixed;
+  bottom: 0;
+  background-color: white;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  box-shadow: 0px -2px 10px rgba(0, 0, 0, 0.1);
+  width: 100%;
+  padding-top: 20px;
+`;
+
 const ItemBox = styled.div`
   display: flex;
   flex-direction: row;
@@ -48,4 +105,5 @@ const ItemBox = styled.div`
   background-color: #f2f2f2;
   color: #555555;
 `;
+
 export default PrevMaterial;
